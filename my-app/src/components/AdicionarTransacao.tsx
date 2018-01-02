ae6e4b2637c86663657072cc339d52f9b3e0cd00
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-01-02 01:04:47 -0200

    adicionando os detalhes da transação

--- my-app/src/components/AdicionarTransacao.tsx
+++ my-app/src/components/AdicionarTransacao.tsx
@@ -5,14 +5,26 @@ export default interface MyProps {
     adicionarTransacao: (conta: number, transacao: Transacao) => {};
 }
 
-export class AdicionarTransacao extends React.Component<MyProps> {
+export class AdicionarTransacao extends React.Component<MyProps, {}> {
 
     handleSubmit(e: any) {
+        const inputNome = (this.refs['nome'] as any as HTMLInputElement)
         const inputConta = (this.refs['conta'] as any as HTMLInputElement)
         const inputValor = (this.refs['valor'] as any as HTMLInputElement)
+        const inputCheckbox = (this.refs['credito'] as any as HTMLInputElement)
 
+        const nome = inputNome.value.trim();
         const conta = inputConta.value.trim();
         const valor = inputValor.value.trim();
+        const isCredito = inputCheckbox.checked;
+
+        if (nome === '') {
+            alert('Nome é um campo obrigatório');
+        }
+
+        if (conta === '') {
+            alert('Conta é um campo obrigatório');
+        }
 
         if (valor === '') {
             alert('Valor é um campo obrigatório');
@@ -21,11 +33,12 @@ export class AdicionarTransacao extends React.Component<MyProps> {
         let contaNumber = parseInt(conta)
         let valorNumber = parseInt(valor)
 
-        let transacao = new Transacao(true, "teste", new Date(), valorNumber)
+        let transacao = new Transacao(isCredito, nome, new Date(), valorNumber)
         this.props.adicionarTransacao(contaNumber, transacao);
 
         e.preventDefault();
 
+        inputNome.value = ''
         inputConta.value = ''
         inputValor.value = ''
     }
@@ -36,6 +49,10 @@ export class AdicionarTransacao extends React.Component<MyProps> {
                 <h3>Adicionar Transação</h3>
                 <form onSubmit={this.handleSubmit.bind(this)}>
                     <div>
+                        <label>Nome</label><br />
+                        <input type="text" ref="nome" />
+                    </div>
+                    <div>
                         <label>Conta</label><br />
                         <input type="text" ref="conta" />
                     </div>
@@ -44,6 +61,11 @@ export class AdicionarTransacao extends React.Component<MyProps> {
                         <input type="text" ref="valor" />
                     </div>
                     <br />
+                    <div>
+                        <label>Crédito: </label><br />
+                        <input type="checkbox" ref="credito" />
+                        <br />
+                    </div>
                     <input type="submit" value="Submit" />
                     <br />
                 </form>
